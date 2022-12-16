30fb861cf162311f3b6c5641e8e73e703202b64d
Mikael Wistoft <mikaelwistoft@gmail.com>
2022-12-16 11:27:25 +0100

    Prune unused test logs

---
+++ packages/dev/scripts/lint-prune-testlogs.ts
@@ -0,0 +1,71 @@
+import fs from "fs";
+import path from "path";
+import { ComposedTestFramework } from "^jates";
+import { TestLogController } from "^jates/TestLogController";
+import { projectRoot } from "^dev/project.conf";
+
+/**
+ *
+ */
+export const doit = async () => {
+  //todo: get this configuration from javi configuration.
+
+  const runners = {
+    ".ja.js": undefined,
+    ".ja.ts": undefined,
+    ".ja.jsx": undefined,
+    ".ja.tsx": undefined,
+    ".pr.js": undefined,
+    ".pr.ts": undefined,
+    ".wo.js": undefined,
+    ".wo.ts": undefined,
+  } as any;
+
+  const absTestFolder = path.join(projectRoot, "packages/tests/");
+  const absTestLogFolder = path.join(projectRoot, "packages/tests/_testLogs");
+
+  //the objects
+
+  const jarun = new ComposedTestFramework({
+    absTestFolder,
+    subFolderIgnore: ["node_modules"],
+    runners,
+  });
+
+  const testLogController = new TestLogController({
+    absTestLogFolder,
+    onError: console.log,
+  });
+
+  //test logs
+
+  const testLogs = await fs.promises.readdir(absTestLogFolder);
+
+  //tests
+
+  const test = await jarun
+    .getTestIds()
+    .then((data) =>
+      data.map((file) =>
+        path.basename((testLogController as any).getExpFilename(file))
+      )
+    );
+
+  const toDelete = difference(testLogs, test);
+
+  for (const file of toDelete) {
+    fs.unlinkSync(path.join(absTestLogFolder, file));
+  }
+
+  console.log(toDelete.length + " files deleted");
+};
+
+/**
+ *
+ */
+const difference = (a: string[], b: string[]) => {
+  const set = new Set(b);
+  return a.filter((x) => !set.has(x));
+};
+
+doit();
--- packages/tests/_slow/jab-node mainWrapper server 2.ja.ts
+++
@@ -1,9 +0,0 @@
-import { TestProvision } from "^jarun";
-import { getJabTsProcess_ready, getScriptPath } from "^tests/_fixture";
-
-//registers on shutdown
-
-export default async (prov: TestProvision) =>
-  getJabTsProcess_ready(prov, {
-    filename: getScriptPath("mainWrapper2"),
-  }).then((proc) => proc.shutdown());
