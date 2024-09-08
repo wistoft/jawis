896091cc22462474e6b723bc686a9feb2d882294
Mikael Wistoft-Ibsen <mikaelwistoft@gmail.com>
2024-09-08 13:52:02 +0200

    Fix flacky test cases

--- packages/tests/all/jadev-console makeUseConsoleStream err 1.ja.ts
+++ packages/tests/all/jadev-console makeUseConsoleStream err 1.ja.ts
@@ -3,6 +3,9 @@ import { TestProvision } from "^jarun";
 
 //there is no global window variable
 
+declare let global: any;
+
 export default (prov: TestProvision) => {
+  delete global.window;
   makeUseConsoleStream();
 };
