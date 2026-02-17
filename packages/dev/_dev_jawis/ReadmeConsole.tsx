cd9473848fe96622b6a571d7031c5b9eb3b8dc16
Mikael Wistoft-Ibsen <>
2026-02-17 16:40:11 +0100

    Fix probleams

--- packages/dev/_dev_jawis/ReadmeConsole.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Console } from "^console";
-
-export const MyComponent: React.FC = () => (
-  <>
-    This is where the console output will appear:
-    <Console />
-  </>
-);
